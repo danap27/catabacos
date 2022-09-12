3524122c1b34e273d87c09db40d3b04288ae3aa1
David Navarrete <davinavarrete@alumnos.uai.cl>
2022-09-12 19:30:45 -0300

    workspace setup

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const port = 3200;
+const app = express();
+
+app.get("/test", (req, res) => {
+  res.send({ data: "Hello World!" });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
